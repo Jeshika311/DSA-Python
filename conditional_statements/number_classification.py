861bdd1ba14625848f57ead13ee7c86171574ef5
jeshika311 <jeshikatha@gmail.com>
2025-01-12 11:32:32 +0530

    add new file

---
+++ conditional_statements/number_classification.py
@@ -0,0 +1,8 @@
+num = float(input("Enter any number : "))
+
+if(num==0):
+    print("Given number is zero")
+elif(num<0):
+    print("Given number is negative")
+elif(num>0):
+    print("Given number is positive")
