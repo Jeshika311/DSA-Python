ac83bf7a9d91a374addefe306fbad21f769624a4
jeshika311 <jeshikatha@gmail.com>
2025-02-03 12:52:06 +0530

    add new file

---
+++ pattren/left_angled_star_pattern.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the number of rows you want : "))
+
+for i in range(n):
+    for j in range(n-i):
+        print(" " , end = " ")
+    for k in range(i):
+        print("*" , end = " ")
+
+    print()
