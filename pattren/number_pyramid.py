c6e68a0021e4fedbdd74f5c6cda5957e97df23ef
jeshika311 <jeshikatha@gmail.com>
2025-02-04 08:33:40 +0530

    add new file

---
+++ pattren/number_pyramid.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the number of rows you want : "))
+p=1
+
+for i in range(n):
+    for j in range(n-i):
+        print(" " , end ="")
+    for k in range(i+1):
+        print(p , end = " ")
+    p+=1
+    print()
