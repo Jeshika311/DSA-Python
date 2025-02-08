09fe2662ecc7ccd713f36c5afc7c9642c8c9c360
jeshika311 <jeshikatha@gmail.com>
2025-02-08 09:39:21 +0530

    add new file

---
+++ pattren/star_rectangle_pattern.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the number of rows you want : "))
+k = int(input("Enter the number of columns you want : "))
+
+for i in range(n):
+    for j in range(k):
+        print("*" , end = " ")
+
+    print()
